f8a575b7d96121244541ba2c5625a24b1fed578f
grunku <rheeskennedy@hotmail.com>
2024-10-18 05:33:25 -0700

    basket working

--- Patches/Basket.cs
+++ Patches/Basket.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Unity.Netcode;
 using UnityEngine;
 
@@ -6,11 +7,9 @@ public class BasketSimplified : AnimatedItem
 
     [Header("Basket Settings")]
 
-    private GrabbableObject heldObject = null;
+    private GrabbableObject basketObject = null;
 
-    private bool holdingObject;
-
-    public GameObject displayObject;
+    public Transform itemHolder;
 
     private DepositItemsDesk desk;
 
@@ -25,37 +24,101 @@ public class BasketSimplified : AnimatedItem
     public override void Update()
     {
         base.Update();
-        if (holdingObject && heldObject.heldByPlayerOnServer)
+        if (basketObject != null)
         {
-            holdingObject = false;
+            if (!basketObject.scrapPersistedThroughRounds && playerHeldBy != null)
+            {
+                if (playerHeldBy.isInHangarShipRoom)
+                {
+                    CollectHeldItem();
+                }
+            }
+
+            if (basketObject.heldByPlayerOnServer)
+            {
+                basketObject = null;
+            }
         }
     }
 
     public override void LateUpdate()
     {
         base.LateUpdate();
-        if (holdingObject && heldObject != null)
+        if (basketObject != null)
         {
-            heldObject.transform.position = displayObject.transform.position;
-            heldObject.transform.rotation = displayObject.transform.rotation;
+            basketObject.transform.position = itemHolder.position;
+            basketObject.transform.rotation = itemHolder.rotation;
         }
+    }
+
+    public override void OnBroughtToShip()
+    {
+        base.OnBroughtToShip();
+        if (basketObject != null)
+        {
+            CollectHeldItem();
+        }
+    }
+
+    public void CollectHeldItem()
+    {
+        if (!basketObject.scrapPersistedThroughRounds)
+        {
+            if (playerHeldBy.isInHangarShipRoom)
+            {
+                RoundManager.Instance.scrapCollectedInLevel += basketObject.scrapValue;
+                StartOfRound.Instance.gameStats.allPlayerStats[playerHeldBy.playerClientId].profitable += basketObject.scrapValue;
+                RoundManager.Instance.CollectNewScrapForThisRound(basketObject);
+                basketObject.OnBroughtToShip();
+
+                if (basketObject.itemProperties.isScrap && Vector3.Distance(GameNetworkManager.Instance.localPlayerController.transform.position, basketObject.transform.position) < 12f)
+                {
+                    HUDManager.Instance.DisplayTip("Got scrap!", "To sell, use the terminal to route the ship to the company building.", isWarning: false, useSave: true, "LCTip_SellScrap");
+                }
+            }
+            else
+            {
+                if (!basketObject.scrapPersistedThroughRounds)
+                {
+                    RoundManager.Instance.scrapCollectedInLevel -= basketObject.scrapValue;
+                    StartOfRound.Instance.gameStats.allPlayerStats[playerHeldBy.playerClientId].profitable -= basketObject.scrapValue;
+                }
+                HUDManager.Instance.SetQuota(RoundManager.Instance.scrapCollectedInLevel);
+            }
+            if (playerHeldBy.isInHangarShipRoom)
+            {
+                StartOfRound.Instance.currentShipItemCount++;
+            }
+            else
+            {
+                StartOfRound.Instance.currentShipItemCount--;
+            }
+        }
+    }
+
+    public IEnumerator DetectDroppedItemHitGround(GrabbableObject gObject)
+    {
+        yield return new WaitUntil(() => gObject.hasHitGround);
+
+        gObject.PlayDropSFX();
     }
 
     public override void GrabItem()
     {
         base.GrabItem();
         base.playerHeldBy.equippedUsableItemQE = true;
-        if (holdingObject)
+        if (basketObject != null)
         {
-            playerHeldBy.carryWeight = Mathf.Clamp(playerHeldBy.carryWeight + (heldObject.itemProperties.weight - 1f), 1f, 10f);
-            if (heldObject.gameObject.GetComponent<AnimatedItem>() != null)
+            basketObject.EnablePhysics(enable: true);
+            playerHeldBy.carryWeight = Mathf.Clamp(playerHeldBy.carryWeight + (basketObject.itemProperties.weight - 1f), 1f, 10f);
+            if (basketObject.gameObject.GetComponent<AnimatedItem>() != null)
             {
                 StartAnimatedItem();
             }
 
-            if (heldObject.gameObject.GetComponent<InteractTrigger>() != null)
+            if (basketObject.gameObject.GetComponent<InteractTrigger>() != null)
             {
-                InteractTrigger trigger = heldObject.gameObject.GetComponent<InteractTrigger>();
+                InteractTrigger trigger = basketObject.gameObject.GetComponent<InteractTrigger>();
                 trigger.interactable = false;
             }
         }
@@ -64,17 +127,18 @@ public class BasketSimplified : AnimatedItem
     public override void DiscardItem()
     {
         base.playerHeldBy.equippedUsableItemQE = false;
-        if (holdingObject)
+        if (basketObject != null)
         {
-            playerHeldBy.carryWeight = Mathf.Clamp(playerHeldBy.carryWeight - (heldObject.itemProperties.weight - 1f), 1f, 10f);
-            if (heldObject.gameObject.GetComponent<AnimatedItem>() != null)
+            basketObject.EnablePhysics(enable: true);
+            playerHeldBy.carryWeight = Mathf.Clamp(playerHeldBy.carryWeight - (basketObject.itemProperties.weight - 1f), 1f, 10f);
+            if (basketObject.gameObject.GetComponent<AnimatedItem>() != null)
             {
                 StopAnimatedItem();
             }
 
-            if (heldObject.gameObject.GetComponent<InteractTrigger>() != null)
+            if (basketObject.gameObject.GetComponent<InteractTrigger>() != null)
             {
-                InteractTrigger trigger = heldObject.gameObject.GetComponent<InteractTrigger>();
+                InteractTrigger trigger = basketObject.gameObject.GetComponent<InteractTrigger>();
                 trigger.interactable = true;
             }
         }
@@ -87,21 +151,21 @@ public class BasketSimplified : AnimatedItem
         Debug.Log("Basket placed.");
         desk = Object.FindObjectOfType<DepositItemsDesk>();
         base.OnPlaceObject();
-        if (heldObject != null)
+        if (basketObject != null)
         {
-            heldObject.grabbable = false;
-            heldObject.grabbableToEnemies = false;
-            heldObject.EnablePhysics(enable: false);
-            if (heldObject.gameObject.GetComponent<AnimatedItem>() != null)
+            basketObject.grabbable = false;
+            basketObject.grabbableToEnemies = false;
+            basketObject.EnablePhysics(enable: true);
+            if (basketObject.gameObject.GetComponent<AnimatedItem>() != null)
             {
                 StopAnimatedItem();
             }
             Debug.Log("Basket placed while holding item.");
 
-            if (desk != null && desk.itemsOnCounter.Contains(this) && !desk.itemsOnCounter.Contains(heldObject))
+            if (desk != null && desk.itemsOnCounter.Contains(this) && !desk.itemsOnCounter.Contains(basketObject))
             {
-                desk.itemsOnCounter.Add(heldObject);
-                desk.itemsOnCounterNetworkObjects.Add(heldObject.gameObject.GetComponent<NetworkObject>());
+                desk.itemsOnCounter.Add(basketObject);
+                desk.itemsOnCounterNetworkObjects.Add(basketObject.gameObject.GetComponent<NetworkObject>());
 
                 for (int i = 0; i < desk.itemsOnCounter.Count; i++)
                 {
@@ -139,7 +203,7 @@ public class BasketSimplified : AnimatedItem
             Debug.Log("Basket !right");
             if (!RequireInteractCooldown())
             {
-                if (!holdingObject)
+                if (basketObject == null)
                 {
                     Debug.Log("No item in basket, attempting detection for item to grab.");
                     if (playerHeldBy == null)
@@ -180,15 +244,17 @@ public class BasketSimplified : AnimatedItem
     {
         //SET PARAMETERS TO INDICATE OBJECT IS BEING HELD
         Debug.Log($"Placing {gObject} into basket.");
-        heldObject = gObject;
-        holdingObject = true;
-        heldObject.isHeld = true;
-
-        if (heldObject.gameObject.GetComponent<InteractTrigger>() != null)
+        basketObject = gObject;
+        basketObject.EnablePhysics(enable: false);
+        if (basketObject.parentObject != null)
         {
-            InteractTrigger trigger = heldObject.gameObject.GetComponent<InteractTrigger>();
-            trigger.interactable = false;
+            basketObject.parentObject = null;
         }
+
+        //PLACE OBJECT INTO BASKET
+        Debug.Log($"Basket position: {transform.position}");
+        Debug.Log($"itemHolder position: {itemHolder.transform.position}");
+        Debug.Log($"Basket object position: {basketObject.transform.position}");
 
         //PLAY BASKET AUDIO
         if (Vector3.Distance(lastPosition, base.transform.position) > 2f)
@@ -210,22 +276,21 @@ public class BasketSimplified : AnimatedItem
         itemAnimator.Play("grabObject");
 
         //FOR ANIMATED ITEMS
-        if (heldObject is AnimatedItem)
+        if (basketObject is AnimatedItem)
         {
             StartAnimatedItem();
         }
 
         //ADD WEIGHT TO PLAYER
-        playerHeldBy.carryWeight = Mathf.Clamp(playerHeldBy.carryWeight + (heldObject.itemProperties.weight - 1f), 1f, 10f);
-        Debug.Log($"Added {heldObject.itemProperties.weight} to player weight.");
+        playerHeldBy.carryWeight = Mathf.Clamp(playerHeldBy.carryWeight + (basketObject.itemProperties.weight - 1f), 1f, 10f);
+        Debug.Log($"Added {basketObject.itemProperties.weight} to player weight.");
     }
 
     private void RemoveObjectFromBasket()
     {
         //CHECK IF HOLDING OBJECT
-        if (!holdingObject)
+        if (basketObject == null)
         {
-            Debug.Log("Not holding object, aborting remove function.");
             return;
         }
 
@@ -237,48 +302,103 @@ public class BasketSimplified : AnimatedItem
         itemAnimator.Play("removeObject");
 
         //FOR ANIMATED ITEMS
-        if (heldObject is AnimatedItem)
+        if (basketObject is AnimatedItem)
         {
             StopAnimatedItem();
         }
 
-        //DROP HELD OBJECT
-        if (isInElevator)
-        {
-            heldObject.gameObject.transform.SetParent(playerHeldBy.playersManager.elevatorTransform, worldPositionStays: true);
-        }
-        else
-        {
-            heldObject.gameObject.transform.SetParent(playerHeldBy.playersManager.propsContainer, worldPositionStays: true);
-        }
-        playerHeldBy.SetItemInElevator(playerHeldBy.isInHangarShipRoom, playerHeldBy.isInElevator, heldObject);
-        heldObject.EnablePhysics(enable: true);
-        heldObject.gameObject.transform.localScale = heldObject.originalScale;
-        heldObject.startFallingPosition = heldObject.transform.parent.InverseTransformPoint(heldObject.transform.position);
-        heldObject.FallToGround(randomizePosition: true);
-        heldObject.fallTime = UnityEngine.Random.Range(-0.3f, 0.05f);
+        //DROP BASKET OBJECT
+        DiscardBasketObject();
 
         //REMOVE WEIGHT OF OBJECT
-        playerHeldBy.carryWeight = Mathf.Clamp(playerHeldBy.carryWeight - (heldObject.itemProperties.weight - 1f), 1f, 10f);
-        Debug.Log($"Removed {heldObject.itemProperties.weight} from player weight.");
-        heldObject = null;
+        playerHeldBy.carryWeight = Mathf.Clamp(playerHeldBy.carryWeight - (basketObject.itemProperties.weight - 1f), 1f, 10f);
 
-        //SET HELD OBJECT CHECKS
-        if (heldObject.gameObject.GetComponent<InteractTrigger>() != null)
+        //PLAY SOUND FOR DROPPED OBJECT
+        GrabbableObject droppedObject = basketObject;
+        StartCoroutine(DetectDroppedItemHitGround(droppedObject));
+
+        basketObject = null;
+    }
+
+    public void DiscardBasketObject()
+    {
+        bool droppedInElevator = false;
+        bool droppedInShipRoom = false;
+        bool droppedInPhysicsRegion = false;
+        Vector3 targetFloorPosition = GetItemFloorPosition();
+
+        //SET FLOOR Y ROTATION
+        int floorYRot = (int)basketObject.transform.localEulerAngles.y;
+
+        //CHECK WHETHER OBJECT ALLOWS DROPPING AHEAD OF PLAYER
+        if (basketObject.itemProperties.allowDroppingAheadOfPlayer && !playerHeldBy.isInElevator)
         {
-            InteractTrigger trigger = heldObject.gameObject.GetComponent<InteractTrigger>();
-            trigger.interactable = true;
+            targetFloorPosition = playerHeldBy.DropItemAheadOfPlayer();
         }
-        
-        holdingObject = false;
-        heldObject.grabbable = true;
-        heldObject.grabbableToEnemies = true;
-        heldObject = null;
+
+        //CHECK IF PLAYER IS IN ELEVATOR (ON SHIP)
+        if (playerHeldBy.isInElevator)
+        {
+            droppedInElevator = true;
+            targetFloorPosition = StartOfRound.Instance.elevatorTransform.InverseTransformPoint(targetFloorPosition);
+            basketObject.transform.SetParent(StartOfRound.Instance.elevatorTransform, worldPositionStays: true);
+            
+            //CHECK IF ITEM IS DROPPED IN SHIP ROOM
+            if (playerHeldBy.isInHangarShipRoom)
+            {
+                droppedInShipRoom = true;
+            }
+        }
+
+        //CHECK IF ITEM IS DROPPED IN PHYSICSREGION
+        Vector3 hitPoint;
+        NetworkObject physicsRegionOfDroppedObject = basketObject.GetPhysicsRegionOfDroppedObject(playerHeldBy, out hitPoint);
+        if (physicsRegionOfDroppedObject != null)
+        {
+            droppedInPhysicsRegion = true;
+            targetFloorPosition = hitPoint;
+            basketObject.transform.SetParent(physicsRegionOfDroppedObject.transform, worldPositionStays: true);
+
+            PlayerPhysicsRegion componentInChildren = physicsRegionOfDroppedObject.GetComponentInChildren<PlayerPhysicsRegion>();
+            if (componentInChildren != null && componentInChildren.allowDroppingItems)
+            {
+                // targetFloorPosition = componentInChildren.transform.InverseTransformPoint(targetFloorPosition);
+                basketObject.transform.SetParent(componentInChildren.physicsTransform, worldPositionStays: true);
+            }
+        }
+
+        if (!droppedInElevator && !droppedInPhysicsRegion)
+        {
+            targetFloorPosition = StartOfRound.Instance.propsContainer.InverseTransformPoint(targetFloorPosition);
+            basketObject.transform.SetParent(StartOfRound.Instance.propsContainer, worldPositionStays: true);
+        }
+
+        basketObject.heldByPlayerOnServer = false;
+        basketObject.parentObject = null;
+        basketObject.isInElevator = droppedInElevator;
+
+        CollectHeldItem();
+
+        basketObject.EnablePhysics(enable: true);
+        basketObject.EnableItemMeshes(enable: true);
+        basketObject.isHeld = false;
+        basketObject.hasHitGround = false;
+        basketObject.fallTime = 0f;
+        basketObject.startFallingPosition = basketObject.transform.parent.InverseTransformPoint(basketObject.transform.position);
+        basketObject.targetFloorPosition = targetFloorPosition + Vector3.up * basketObject.itemProperties.verticalOffset - Vector3.up * itemProperties.verticalOffset;
+        basketObject.floorYRot = floorYRot;
+
+        Debug.Log($"Basket object start falling position: {basketObject.startFallingPosition}");
+        Debug.Log($"Basket object target floor position: {basketObject.targetFloorPosition}");
+        Debug.Log($"Basket object parentObject: {basketObject.parentObject}");
+        Debug.Log($"Basket object parent: {basketObject.transform.parent}");
+        Debug.Log($"Dropped in ship room: {droppedInShipRoom}");
+        Debug.Log($"Dropped in elevator: {droppedInElevator}");
     }
 
     public void StartAnimatedItem()
     {
-        AnimatedItem aObject = heldObject as AnimatedItem;
+        AnimatedItem aObject = basketObject as AnimatedItem;
         if (aObject.chanceToTriggerAlternateMesh > 0)
         {
             if (aObject.itemRandomChance.Next(0, 100) < aObject.chanceToTriggerAlternateMesh)
@@ -305,7 +425,7 @@ public class BasketSimplified : AnimatedItem
 
     public void StopAnimatedItem()
     {
-        AnimatedItem aObject = heldObject as AnimatedItem;
+        AnimatedItem aObject = basketObject as AnimatedItem;
         if (aObject.itemAnimator != null)
         {
             aObject.itemAnimator.SetBool(aObject.grabItemBoolString, value: false);
