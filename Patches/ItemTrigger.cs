6b3eeee53583e0d036420115bfcf5684e24cb23d
grunku <rheeskennedy@hotmail.com>
2024-10-19 04:14:43 -0700

    new and improved itemtrigger

--- Patches/ItemTrigger.cs
+++ Patches/ItemTrigger.cs
@@ -1,27 +1,62 @@
 using System;
-using System.Collections;
-using GameNetcodeStuff;
-using Unity.Netcode;
+using System.Reflection;
 using UnityEngine;
 
 public class ItemTrigger : MonoBehaviour
 {
-    public GameObject parentObject;
+    public GrabbableObject objectScript;
 
-    public ITouchable itemComponent;
+    public String callOnTriggerEnter;
 
-    private void Start()
+    public String callOnTriggerExit;
+
+    private MethodInfo onEnter;
+
+    private ParameterInfo[] onEnterParameters;
+
+    private MethodInfo onExit;
+
+    private ParameterInfo[] onExitParameters;
+
+    public void Start()
     {
-        itemComponent = parentObject.GetComponent<ITouchable>();
+        // itemComponent = parentObject.GetComponent<ITouchable>();
+        Type itemType = objectScript.GetType();
+
+        onEnter = itemType.GetMethod(callOnTriggerEnter);
+        onEnterParameters = onEnter.GetParameters();
+
+        onExit = itemType.GetMethod(callOnTriggerExit);
+        onExitParameters = onExit.GetParameters();
     }
     
     public void OnTriggerEnter(Collider other)
     {
-        itemComponent.OnTouch(other);
+        // itemComponent.OnTouch(other);
+        if (onEnterParameters.Length == 0)
+        {
+            onEnter.Invoke(objectScript, null);
+        }
+        else
+        {
+            object[] parametersArray = new object[1];
+            parametersArray[0] = other;
+            onEnter.Invoke(objectScript, parametersArray);
+        }
     }
 
     public void OnTriggerExit(Collider other)
     {
-        itemComponent.OnExit(other);
+        // itemComponent.OnExit(other);
+        if (onExitParameters.Length == 0)
+        {
+            onExit.Invoke(objectScript, null);
+        }
+        else
+        {
+            object[] parametersArray = new object[1];
+            parametersArray[0] = other;
+            onExit.Invoke(objectScript, parametersArray);
+        }
     }
 }
