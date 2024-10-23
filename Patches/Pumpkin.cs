d0a8312a94f5140294c5fc0d4e3a5990c94b4c0b
grunku <rheeskennedy@hotmail.com>
2024-10-23 04:34:43 -0700

    pumpkin item

---
+++ Patches/Pumpkin.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using GameNetcodeStuff;
+using Unity.Netcode;
+using UnityEngine;
+
+public class Pumpkin : AnimatedItem
+{
+    [Header("Pumpkin Settings")]
+    public float rotAmount;
+
+}
