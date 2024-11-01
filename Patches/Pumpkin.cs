7051405fc53124a8dfcd822f21aa9387678ac320
grunku <rheeskennedy@hotmail.com>
2024-10-31 18:36:34 -0700

    pumpkin animator set

--- Patches/Pumpkin.cs
+++ Patches/Pumpkin.cs
@@ -1,12 +1,14 @@
-using System;
-using System.Collections;
-using GameNetcodeStuff;
-using Unity.Netcode;
 using UnityEngine;
 
 public class Pumpkin : AnimatedItem
 {
     [Header("Pumpkin Settings")]
     public float rotAmount;
+
+    public override void Start()
+    {
+        base.Start();
+        itemAnimator.SetFloat("rot", rotAmount);
+    }
 
 }
