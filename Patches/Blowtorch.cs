8caca9f6098191dd9edbcf4aaa481f06978e0ede
grunku <rheeskennedy@hotmail.com>
2024-11-02 23:55:11 -0700

    blowtorch cleanup

--- Patches/Blowtorch.cs
+++ Patches/Blowtorch.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Collections;
 using GameNetcodeStuff;
-using Unity.Netcode;
 using UnityEngine;
 
 public class Blowtorch : AnimatedItem
@@ -14,57 +11,25 @@ public class Blowtorch : AnimatedItem
 
     public GameObject rangeEnd;
 
-    private Animator animator;
-
-    private bool isHoldingButton;
-
-    private bool isOn;
-
-    private PlayerControllerB previousPlayerHeldBy;
-
-    public GameObject blowtorchLight;
-
-    [Space(5f)]
-    public AudioSource torchAudio;
-
-    public AudioClip[] on;
-
-    public AudioClip[] off;
-
-    public AudioClip[] fireLoop;
-
     public override void Start()
     {
         base.Start();
-        animator = base.gameObject.GetComponent<Animator>();
     }
 
     public override void ItemActivate(bool used, bool buttonDown = true)
     {
         Debug.Log("ItemActivate called for blowtorch.");
         base.ItemActivate(used, buttonDown);
-        isHoldingButton = buttonDown;
         if (buttonDown)
         {
-            Debug.Log($"Blowtorch isBeingUsed: {isBeingUsed}, buttonDown: {buttonDown}");
-            previousPlayerHeldBy = playerHeldBy;
-            if (!isOn)
-            {
-                turnOn();
-                return;
-            }
-            if (isOn)
-            {
-                turnOff();
-                return;
-            }
+            AnimatorStateInfo state = itemAnimator.GetCurrentAnimatorStateInfo(0);
+            
+            itemAnimator.SetTrigger("Used");
         }
     }
 
-    public IEnumerator delayTorchOn(float delay)
+    public void DamageWithFlame(int force)
     {
-        yield return new WaitForSeconds(delay);
-
         //CHECK FOR COLLIDERS
         Collider[] colliders = checkColliders();
         for (int i = 0; i < colliders.Length; i++)
@@ -94,27 +59,10 @@ public class Blowtorch : AnimatedItem
                 }
             }
         }
-
-        blowtorchLight.SetActive(true);
-
-        //FIGURE OUT HOW TO MAKE THIS LOOP
-        torchAudio.loop = true;
-        RoundManager.PlayRandomClip(torchAudio, fireLoop, randomize: true, 1f, -1);
     }
 
-    public IEnumerator delayTorchOff(float delay)
+    public void MakeNoise()
     {
-        yield return new WaitForSeconds(delay);
-        blowtorchLight.SetActive(false);
-        isOn = false;
-    }
-
-    public void turnOn()
-    {
-        Debug.Log("turnOn called for blowtorch.");
-        isOn = true;
-        animator.Play("turnOn");
-
         if (Vector3.Distance(lastPosition, base.transform.position) > 2f)
         {
             timesPlayedInOneSpot = 0;
@@ -123,28 +71,6 @@ public class Blowtorch : AnimatedItem
         lastPosition = base.transform.position;
 
         RoundManager.Instance.PlayAudibleNoise(base.transform.position, noiseRange, noiseLoudness, timesPlayedInOneSpot, isInShipRoom && StartOfRound.Instance.hangarDoorsClosed);
-        RoundManager.PlayRandomClip(torchAudio, on, randomize: true, 1f, -1);
-
-        StartCoroutine(delayTorchOn(on[0].length));
-    }
-
-    public void turnOff()
-    {
-        Debug.Log("turnOff called for blowtorch.");
-        animator.Play("turnOff");
-        
-        if (Vector3.Distance(lastPosition, base.transform.position) > 2f)
-        {
-            timesPlayedInOneSpot = 0;
-        }
-        timesPlayedInOneSpot++;
-        lastPosition = base.transform.position;
-
-        torchAudio.loop = false;
-        RoundManager.Instance.PlayAudibleNoise(base.transform.position, noiseRange, noiseLoudness, timesPlayedInOneSpot, isInShipRoom && StartOfRound.Instance.hangarDoorsClosed);
-        RoundManager.PlayRandomClip(torchAudio, off, randomize: true, 1f, -1);
-
-        StartCoroutine(delayTorchOff(off[0].length));
     }
 
     private Collider[] checkColliders()
