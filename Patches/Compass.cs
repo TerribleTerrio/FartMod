95529e81c5854e0ba547f0deb1d13a0e9bd1fb84
grunku <rheeskennedy@hotmail.com>
2024-11-04 15:40:04 -0800

    compass syncing

--- Patches/Compass.cs
+++ Patches/Compass.cs
@@ -1,140 +1,180 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using GameNetcodeStuff;
+using Unity.Netcode;
 using UnityEngine;
 
 public class Compass : AnimatedItem, IHittable
 {
-    
+    [Space(15f)]
     [Header("Compass Settings")]
     public float detectRange;
 
-    public float alertRange;
+    private bool active;
 
-    public float alertCooldown;
+    private GameObject closestObject;
 
-    private bool alertOnCooldown;
+    private float closestObjectDistance;
 
-    private bool broken;
+    private Coroutine loadItemsCoroutine;
+
+    private Coroutine changeOffsetCoroutine;
 
     public Item[] detectableItems;
 
-    public List<GrabbableObject> detectedItems;
+    private List<GrabbableObject> detectedObjects = new List<GrabbableObject>();
 
-    [Space(5f)]
-    public AudioClip[] alert;
+    public GameObject needleBone;
 
-    public AudioClip[] breakSFX;
+    private float rotationCurrent;
+
+    private float offset;
+
+    private float targetOffset;
+
+    [Space(10f)]
+    [Header("Audio")]
+    public AudioSource noiseAudio;
 
     public override void Start()
     {
         base.Start();
-        LoadDetectedItems();
-
-        if (broken)
-        {
-            itemAnimator.Play("broken");
-        }
+        rotationCurrent = transform.eulerAngles.y;
     }
 
     public override void Update()
     {
         base.Update();
 
-        if (broken)
+        // SetDirection();
+
+        if (active && loadItemsCoroutine == null && base.IsOwner)
         {
-            return;
+            LoadDetectedObjectsServerRpc();
+            loadItemsCoroutine = StartCoroutine(Cooldown(Random.Range(2f, 5f), loadItemsCoroutine));
         }
 
-        if (detectedItems.Count > 0)
+        if (active && changeOffsetCoroutine == null && base.IsOwner)
         {
-            for (int i = 0; i < detectedItems.Count; i++)
-            {
-                if (detectedItems[i].gameObject == null)
-                {
-                    detectedItems.Remove(detectedItems[i]);
-                }
-            }
-
-            if (ClosestItem() != null)
-            {
-                float distance = Vector3.Distance(ClosestItem().transform.position, base.transform.position);
-                itemAnimator.SetFloat("spinSpeed", Remap(distance, 0, detectRange, 5, 0));
-
-                if (Vector3.Distance(ClosestItem().transform.position, base.transform.position) < alertRange)
-                {
-                    if (!alertOnCooldown)
-                    {
-                        Alert();
-                    }
-                }
-            }
+            ChangeOffsetServerRpc();
+            changeOffsetCoroutine = StartCoroutine(Cooldown(Random.Range(30f,120f), changeOffsetCoroutine));
         }
 
+        float noiseAmount = itemAnimator.GetFloat("NoiseAmount");
+        float targetNoiseAmount;
+
+        if (closestObject != null)
+        {
+            targetNoiseAmount = Remap(closestObjectDistance, 0f, detectRange, 1f, 0f);
+        }
         else
         {
-            itemAnimator.SetFloat("spinSpeed", 0);
+            targetNoiseAmount = 0f;
         }
+
+        noiseAmount = Mathf.Lerp(noiseAmount, targetNoiseAmount, Time.deltaTime);
+        itemAnimator.SetLayerWeight(1, noiseAmount);
+        noiseAudio.volume = noiseAmount;
+
+        offset = Mathf.Lerp(offset, targetOffset, Time.deltaTime);
+
+        needleBone.transform.localEulerAngles = new Vector3(0f, -transform.eulerAngles.y + offset, 0f);
     }
 
-    public void OnEnterTrigger(Collider other = null)
+    [ServerRpc]
+    public void LoadDetectedObjectsServerRpc()
     {
-        if (other == null || other.gameObject.GetComponent<PlayerControllerB>() != null)
+        detectedObjects.Clear();
+        GrabbableObject[] objectsInMap = Object.FindObjectsOfType<GrabbableObject>();
+        for (int i = 0; i < objectsInMap.Length; i++)
         {
-            LoadDetectedItems();
-        }
-    }
-
-    public override void GrabItem()
-    {
-        base.GrabItem();
-        LoadDetectedItems();
-    }
-
-    public void LoadDetectedItems()
-    {
-        GrabbableObject[] allItems = FindObjectsOfType<GrabbableObject>();
-        detectedItems = allItems.ToList();
-
-        for (int i = 0; i < detectedItems.Count; i++)
-        {
-            bool detected = false;
+            GrabbableObject gObject = objectsInMap[i];
             for (int j = 0; j < detectableItems.Length; j++)
             {
-                if (detectedItems[i].itemProperties.itemName != detectableItems[j].itemName)
+                if (gObject.itemProperties.itemName == detectableItems[j].itemName)
                 {
-                    continue;
+                    detectedObjects.Add(gObject);
+                    Debug.Log($"Compass found {gObject}!");
                 }
-                else
-                {
-                    detected = true;
-                }
-            }
-            if (!detected)
-            {
-                detectedItems.Remove(detectedItems[i]);
             }
         }
+
+        ClosestObject().GetComponent<NetworkObject>();
     }
 
-    public GameObject ClosestItem()
+    [ClientRpc]
+    public void LoadDetectedObjectsClientRpc()
     {
-        if (detectedItems.Count > 0 && detectedItems[0] != null)
+        detectedObjects.Clear();
+        GrabbableObject[] objectsInMap = Object.FindObjectsOfType<GrabbableObject>();
+        for (int i = 0; i < objectsInMap.Length; i++)
         {
-            GameObject closestItem = detectedItems[0].gameObject;
-            for (int i = 0; i < detectedItems.Count; i++)
+            GrabbableObject gObject = objectsInMap[i];
+            for (int j = 0; j < detectableItems.Length; j++)
             {
-                if (detectedItems[i] != null)
+                if (gObject.itemProperties.itemName == detectableItems[j].itemName)
                 {
-                    if (Vector3.Distance(detectedItems[i].transform.position, base.transform.position) < Vector3.Distance(closestItem.transform.position, base.transform.position))
-                    {
-                        closestItem = detectedItems[i].gameObject;
-                    }
+                    detectedObjects.Add(gObject);
+                    Debug.Log($"Compass found {gObject}!");
                 }
             }
-            return closestItem;
+        }
+
+        closestObject = ClosestObject();
+    }
+
+    [ServerRpc]
+    public void ChangeOffsetServerRpc()
+    {
+        float offset = Random.Range(0f,360f);
+        ChangeOffsetClientRpc(offset);
+    }
+
+    [ClientRpc]
+    public void ChangeOffsetClientRpc(float offset)
+    {
+        itemAnimator.SetTrigger("Spin");
+        targetOffset = offset;
+        changeOffsetCoroutine = StartCoroutine(Cooldown(Random.Range(30f,120f), changeOffsetCoroutine));
+    }
+
+    // public void SetDirection()
+    // {
+    //     if (rotationPrevious > 270f && transform.eulerAngles.y < 90f)
+    //     {
+    //         rotations++;
+    //     }
+
+    //     if (rotationPrevious < 90f && transform.eulerAngles.y > 270f)
+    //     {
+    //         rotations--;
+    //     }
+
+    //     rotationPrevious = rotationCurrent;
+
+    //     float rotationTarget = rotations*360f + transform.eulerAngles.y;
+
+    //     rotationCurrent = Mathf.Lerp(rotationCurrent, rotationTarget, Time.deltaTime);
+    //     needleBone.transform.localEulerAngles = new Vector3(0f, rotationCurrent, 0f);
+    // }
+
+    public GameObject ClosestObject()
+    {
+        if (detectedObjects.Count > 0)
+        {
+            GameObject closestObjectSoFar = detectedObjects[0].gameObject;
+            float checkObjectDistance = Vector3.Distance(detectedObjects[0].transform.position, base.transform.position);
+            for (int i = 1; i < detectedObjects.Count; i++)
+            {
+                if (Vector3.Distance(detectedObjects[i].transform.position, transform.position) < checkObjectDistance)
+                {
+                    closestObjectSoFar = detectedObjects[i].gameObject;
+                    checkObjectDistance = Vector3.Distance(detectedObjects[i].transform.position, transform.position);
+                }
+            }
+
+            Debug.Log($"Compass closest object: {closestObject}");
+            return closestObjectSoFar;
         }
         else
         {
@@ -142,13 +182,35 @@ public class Compass : AnimatedItem, IHittable
         }
     }
 
-    public void Alert()
+    private IEnumerator Cooldown(float time, Coroutine cooldown)
     {
-        Debug.Log($"Alert triggered by {ClosestItem()}.");
-        alertOnCooldown = true;
-        StartCoroutine(AlertTimer(alertCooldown));
+        yield return new WaitForSeconds(time);
+        cooldown = null;
+    }
 
-        //ALERT SOUND
+    public void OnTriggerStay(Collider other)
+    {
+        PlayerControllerB player = other.gameObject.GetComponent<PlayerControllerB>();
+
+        if (player != null)
+        {
+            active = true;
+        }
+    }
+
+    public override void GrabItem()
+    {
+        base.GrabItem();
+        if (base.IsOwner)
+        {
+            LoadDetectedObjectsServerRpc();
+        }
+    }
+
+    bool IHittable.Hit(int force, Vector3 hitDirection, PlayerControllerB playerWhoHit = null, bool playHitSFX = true, int hitID = -1)
+	{
+        itemAnimator.SetTrigger("Spin");
+
         if (Vector3.Distance(lastPosition, base.transform.position) > 2f)
         {
             timesPlayedInOneSpot = 0;
@@ -157,34 +219,6 @@ public class Compass : AnimatedItem, IHittable
         lastPosition = base.transform.position;
 
         RoundManager.Instance.PlayAudibleNoise(base.transform.position, noiseRange, noiseLoudness, timesPlayedInOneSpot, isInShipRoom && StartOfRound.Instance.hangarDoorsClosed);
-        RoundManager.PlayRandomClip(itemAudio, alert, randomize: true, 1f, -1);
-
-        itemAnimator.Play("alert");
-    }
-
-    public IEnumerator AlertTimer(float seconds)
-    {
-        yield return new WaitForSeconds(seconds);
-        alertOnCooldown = false;
-    }
-
-    bool IHittable.Hit(int force, Vector3 hitDirection, PlayerControllerB playerWhoHit = null, bool playHitSFX = true, int hitID = -1)
-	{
-        if (!broken)
-        {
-            broken = true;
-            itemAnimator.SetBool("broken", true);
-
-            if (Vector3.Distance(lastPosition, base.transform.position) > 2f)
-            {
-                timesPlayedInOneSpot = 0;
-            }
-            timesPlayedInOneSpot++;
-            lastPosition = base.transform.position;
-
-            RoundManager.Instance.PlayAudibleNoise(base.transform.position, noiseRange, noiseLoudness, timesPlayedInOneSpot, isInShipRoom && StartOfRound.Instance.hangarDoorsClosed);
-            RoundManager.PlayRandomClip(itemAudio, breakSFX, randomize: true, 1f, -1);
-        }
         
         return true;
 	}
